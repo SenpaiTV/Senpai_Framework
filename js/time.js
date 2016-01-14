1673ee7df1b76e36199c5a032a116a6d421786d8
Marc Sollie <ik@marcsollie.nl>
2016-01-14 15:08:48 +0100

    Object for the classes

--- js/time.js
+++ js/time.js
@@ -1,11 +1,18 @@
 /**
  * Created by marcs on 14-1-2016.
  */
-var POSITIONS = 2;
+
+
+function Klasse(naam, time){
+    this.time = time;
+    this.naam = naam;
+}
+var POSITIONS = new Array(new Klasse("test", 1000), new Klasse("test", 1000));
 
 function onTime(){
     var x = Math.floor(Math.random() * POSITIONS);
 }
+
 window.onload(new function(){
     window.setInterval(onTime, 1000);
 });
