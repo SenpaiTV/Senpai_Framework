9a025dcc96b8939ed6de906448dbe1b20042c845
Marc Sollie <marc@marcsollie.nl>
2016-01-14 14:50:53 +0100

    Basics
    Document onload

--- js/time.js
+++ js/time.js
@@ -1,3 +1,11 @@
 /**
  * Created by marcs on 14-1-2016.
  */
+var POSITIONS = 2;
+
+function onTime(){
+    var x = Math.floor(Math.random() * POSITIONS);
+}
+window.onload(new function(){
+    window.setInterval(onTime, 1000);
+});
