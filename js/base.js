e751af25cb15cd665bc1a3a5ba1da3c56ca73e7f
Marc Sollie <ik@marcsollie.nl>
2016-01-14 15:15:07 +0100

    Dingetjes
    In meerdere stylesheets verdeeld, index.html img ID gegeven

---
+++ js/base.js
@@ -0,0 +1,11 @@
+/**
+ * Created by marcs on 14-1-2016.
+ */
+
+var POSITIONS = new Array(new CSSClass("bigger", 1000), new CSSClass("smaller", 1000));
+
+
+
+window.onload(new function(){
+    onTime();
+});
--- js/time.js
+++ js/time.js
@@ -3,16 +3,12 @@
  */
 
 
-function Klasse(naam, time){
-    this.time = time;
-    this.naam = naam;
-}
-var POSITIONS = new Array(new Klasse("test", 1000), new Klasse("test", 1000));
+
 
 function onTime(){
     var x = Math.floor(Math.random() * POSITIONS);
-}
-
-window.onload(new function(){
     window.setInterval(onTime, 1000);
-});
+    var css = POSITIONS[x];
+    document.getElementById("")
+    css.naam
+}
