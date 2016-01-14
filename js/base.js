b6be2dbb1613d9381509978dd68d67192d5cc674
Marc Sollie <ik@marcsollie.nl>
2016-01-14 16:22:00 +0100

    Window onload function naar base verplaatst + config

--- js/base.js
+++ js/base.js
@@ -3,5 +3,21 @@
  */
 
 var POSITIONS = new Array(new CSSClass("bigger", 1000), new CSSClass("smaller", 1000));
+var audio = new Audio(audioSrc);
+function loopAudio(){
+    audio.loop = true;
+    audio.play();
+}
+function onLoaded(){
+    counter = document.getElementById("counter");
+    image = document.getElementById("afbeelding");
+
+    onAnimationTime();
+
+    window.setTimeout(onNextSecond, 1000 );
+    console.log("Window Loaded!");
+    loopAudio();
+    gabifyConsole();
 
 
+};
