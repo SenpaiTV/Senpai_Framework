733319d335dbe58028cfbbf0e2f03096d4b92ff9
Marc Sollie <marc@marcsollie.nl>
2016-01-15 20:21:47 +0100

    Kijkt nu ook echt naar imageSrc parameter

--- js/base.js
+++ js/base.js
@@ -11,11 +11,11 @@ function onLoaded(){
     console.log("Window Loaded!");
     counter = document.getElementById("counter");
     image = document.getElementById("afbeelding");
-
+    image.src = imageSrc;
     onAnimationStart();
     initAudio();
     window.setTimeout(onNextSecond, 1000 );
-    //gabifyConsole();
+    gabifyConsole();
 
 
 };
